893d1040b125ef296b99905d106fbe70749ac663
Георгий Либиков <armhaina@mail.ru>
2026-05-12 22:49:19 +0300

    app

--- src/Element/Handler/Clr.php
+++ src/Element/Handler/Clr.php
@@ -37,7 +37,7 @@ readonly class Clr
         $propertiesSlc = [];
 
         foreach ($properties as $field => $property) {
-            if ($property['VALUE'] === '') {
+            if ($property['VALUE'] === '' || $property['VALUE'] === null || $property['VALUE'] === false) {
                 continue;
             }
 
